22fbdaa10b1989c5bbeec721f54fe1d379d04c1a
Phsnomy <anec2003@outlook.com>
2021-02-16 19:52:55 +0800

    oAuth implemention

--- src/middleware/oauth.ts
+++ src/middleware/oauth.ts
@@ -1,34 +1,51 @@
 import express = require("express")
-import axios from "axios"
+import axios, { AxiosRequestConfig } from "axios"
 import config from "../snippets/config"
-import { type } from "os"
 
-let user: {
-    username: string,
-    avatar: string
+let user= {
+    username: 'none',
+    avatar: 'none',
+    id: 0,
+    email: 'none',
+    href: 'none'
 }
 
-let oauth = (req: express.Request, res: express.Response) => {
+function oauth(req: express.Request, res: express.Response) {
     let code = req.query.code
-    axios({
+    let resconf: AxiosRequestConfig = {
         method: 'post',
         url: 'https://github.com/login/oauth/access_token?' +
             `client_id=${config.clientid}&` +
             `client_secret=${config.clientsecret}&` +
             `code=${code}`,
         headers: { accept: 'application/json' }
-    }).then((oresfirst) => {
+    }
+    axios(resconf).then(async (oresfirst) => {
         // oresfirst: oAuth first response
-        axios({
-            method: 'get',
-            url: 'https://api.github.com/user',
-            headers: { accept: 'application/json',
-            Authorization: `token ${oresfirst.data.access_token}`}}).then((oresecond)=>{
-                // oresecond: oAuth second response
-                console.log(oresecond.data)
-            }).catch((reason)=>{console.log(reason)})
-    }).catch((reason) => {console.log(reason)})
-    res.send("OK")
+        resconf.method = 'get'
+        resconf.url = 'https://api.github.com/user'
+        resconf.headers = {
+            accept: 'application/json',
+            Authorization: `token ${oresfirst.data.access_token}`
+        }
+        // oresecond: oAuth second response
+        await axios(resconf).then((oresecond) => {
+            user.username = oresecond.data.login
+            user.id = oresecond.data.id
+            user.avatar = oresecond.data.avatar
+            user.href = oresecond.data.html_url
+        })
+        resconf.url = 'https://api.github.com/user/emails'
+        await axios(resconf).then((oresecond) => {
+            user.email = oresecond.data[0].email
+        })
+        // user is now aviliable
+        res.json(user)
+        // write to database
+    }).catch((reason)=>{
+        // Should have error message
+        res.send("LoginError")
+    })
 }
 
 export = oauth
